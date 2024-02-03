b2d0040e4b9a94a068860e4798e61e509e4ac1f5
Matt Jones <me@mattjones.tech>
2024-02-03 10:21:30 -0500

    lesson 8, php functions

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,10 @@
-This is our completely amazing new theme.
+<?
+function greet($name, $color) {
+  echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
+}
+
+greet('John', 'green');
+greet('Jane', 'blue');
+?>
+  <h1><? bloginfo('name'); ?></h1>
+  <h1><? bloginfo('description'); ?></h1>
