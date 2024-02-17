57a66771f53629e57bea24020344426e2686c69f
Matt Jones <me@mattjones.tech>
2024-02-17 13:28:44 -0500

    finish lesson 20, nav menus

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -12,23 +12,18 @@
             <div class="site-footer__col-two">
               <h3 class="headline headline--small">Explore</h3>
               <nav class="nav-list">
-                <ul>
-                  <li><a href="<? echo site_url('/about-us'); ?>">About Us</a></li>
-                  <li><a href="#">Programs</a></li>
-                  <li><a href="#">Events</a></li>
-                  <li><a href="#">Campuses</a></li>
-                </ul>
+<? wp_nav_menu([
+  'theme_location' => 'footerLocationOne'
+]); ?>
               </nav>
             </div>
 
             <div class="site-footer__col-three">
               <h3 class="headline headline--small">Learn</h3>
               <nav class="nav-list">
-                <ul>
-                  <li><a href="#">Legal</a></li>
-                  <li><a href="<? echo site_url('/privacy-policy'); ?>">Privacy</a></li>
-                  <li><a href="#">Careers</a></li>
-                </ul>
+ <? wp_nav_menu([
+  'theme_location' => 'footerLocationTwo'
+]); ?>
               </nav>
             </div>
           </div>
