5e73220572b284a019c8b2545e08c94c0e5c27b2
Matt Jones <me@mattjones.tech>
2024-02-21 19:20:45 -0500

    lesson 26, various improvements

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -64,7 +64,7 @@ while($homepagePosts->have_posts()) {
 wp_reset_postdata();
 ?>
           
-          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+  <p class="t-center no-margin"><a href="<? echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
       </div>
     </div>
